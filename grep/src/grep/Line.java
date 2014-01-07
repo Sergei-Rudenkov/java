fe3d0d1e7d2052dfa3eb981e9ed0c44974f91091
iResNovae <iresnovaeserg@gmail.com>
2014-01-08 00:17:31 +0300

    grep

---
+++ grep/src/grep/Line.java
@@ -0,0 +1,39 @@
+package grep;
+
+import java.util.*;
+
+
+public class Line {
+	String line, word;
+	int times = 0;
+	Scanner scn = new Scanner(System.in);
+	 List<String> lines = new ArrayList<String>();
+	void getword(){
+		System.out.println("Введети ключевое слово");
+		word = scn.nextLine(); 
+	}
+	
+	void search(){
+		
+		Iterator<String> it = lines.iterator();
+		while(it.hasNext()) {
+			String s = it.next();
+			Boolean found = Arrays.asList(line.split(" ")).contains(word);
+			if(found){
+			      System.out.println("Keyword matched the string");
+			}
+		}
+	}
+		
+	void printout(){
+		
+	}
+		public void Scaning() {	
+			for( int times = 0; times < 2; times++){ 
+			System.out.println("Веведите строку");
+			 line = scn.nextLine();
+			 lines.add(line);
+			}
+		}	
+}
+
