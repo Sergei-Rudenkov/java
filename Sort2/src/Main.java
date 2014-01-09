c231b7579ab848b514c429f945d875d25860951d
iResNovae <iresnovaeserg@gmail.com>
2014-01-09 16:45:50 +0300

    sort by length

--- Sort2/src/Main.java
+++ Sort2/src/Main.java
@@ -1,71 +1,71 @@
+package test;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.Comparator;
 
-public class Main
-{
-//вспомогательный класс, реализующий удобный нам формат строк
-static private abstract class MyString implements Comparable {
-private String interString;
-private int sortIndex = -1;
-public MyString(String str, int wordNum)
-{
-this.interString = str;
-this.sortIndex = wordNum;
-}
-public String getStr()
-{
-return interString;
-}
+public class ArrayListSortingExample {
 
-public int compareTo(MyString arg0)
-{
-//сортировка без учёта регистра
-//return str.toLowerCase().compareTo(arg0.getStr().toLowerCase());
-//сортировка по длинне (будет выполняться елси параметр не задан)
-if (sortIndex == -1) return interString.length() - arg0.getStr().length();
-//сортировка по sortIndex`овому слову в строке
-String str1 = null, str2 = null;
-try
-{
-str1 = interString.split(" ")[sortIndex];
-}
-catch (Exception e) 
-{
-//если нет нужного слова в 1ой строке, то считаем что первое слово меньше 2ого.
-return -1;
-}
-try
-{
-str2 = arg0.getStr().split(" ")[sortIndex];
-}
-catch (Exception e) 
-{
-//если нет нужного слова во 2ой строке, то считаем что 2ое слово меньше 1ого
-return 1;
-}
-//здесь сравниваем корректно
-return str1.compareTo(str2);
-}
-};
-// точка входа в программу
-public static void main(String[] args)
-{
-//устанавливаем номер слова в строке для сортировки (если не указал, то -1) 
-int sortIndex = -1;
-//проверяем кол-во аргументов
-if (args.length != 0) sortIndex = Integer.valueOf(args[0]);
-Scanner in = new Scanner(System.in);
-ArrayList.sortList = new ArrayList();
-//начинаем разбор по строчкам, и заполнение объекта контейнера
-while (in.hasNext())
-sortList.add(new MyString(in.nextLine(), sortIndex));
-Collections.sort(sortList);
-//печать на экранчик
-Iteratoriter = sortList.iterator();
-while (iter.hasNext())
-System.out.println(iter.next().getStr());
-in.close();
-}
+   private static class SmartPhone implements Comparable {
+        private String brand;
+        private String model;
+        private int price;
+
+        public SmartPhone(String brand, String model, int price){
+            this.brand = brand;
+            this.model = model;
+            this.price = price;
+        }
+      
+        @Override
+        public int compareTo(SmartPhone sp) {
+            return this.brand.compareTo(sp.brand);
+        }
+
+        @Override
+        public String toString() {
+            return "SmartPhone{" + "brand=" + brand + ", model=" + model + ", price=" + price + '}';
+        }
+      
+    }
+  
+    private static class PriceComparator implements Comparator{
+
+        @Override
+        public int compare(SmartPhone sp1, SmartPhone sp2) {
+            return (sp1.price < sp2.price ) ? -1: (sp1.price > sp2.price) ? 1:0 ;
+        }
+      
+    }
+
+    public static void main(String... args) {
+      
+        //creating objects for arraylist sorting example
+        SmartPhone apple = new SmartPhone("Apple", "IPhone4S",1000);
+        SmartPhone nokia = new SmartPhone("Nokia", "Lumia 800",600);
+        SmartPhone samsung = new SmartPhone("Samsung", "Galaxy Ace",800);
+        SmartPhone lg = new SmartPhone("LG", "Optimus",500);
+      
+        //creating Arraylist for sorting example
+        ArrayList smartPhones = new ArrayList();
+      
+        //storing objects into ArrayList for sorting
+        smartPhones.add(apple);
+        smartPhones.add(nokia);
+        smartPhones.add(samsung);
+        smartPhones.add(lg);
+      
+        //Sorting Arraylist in Java on natural order of object
+        Collections.sort(smartPhones);
+      
+        //print sorted arraylist on natural order
+        System.out.println(smartPhones);
+      
+        //Sorting Arraylist in Java on custom order defined by Comparator
+        Collections.sort(smartPhones,new PriceComparator());
+      
+        //print sorted arraylist on custom order
+        System.out.println(smartPhones);
+    
+    }
 }
