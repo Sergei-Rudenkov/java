06ccd0b07c231d2f7cb56df77a0aa5234e879515
iResNovae <iresnovaeserg@gmail.com>
2014-01-12 01:57:02 +0300

    epam

---
+++ epam/src/epam/Sentence.java
@@ -0,0 +1,13 @@
+package epam;
+
+public class Sentence extends MainClass {
+static String text;
+
+	void create(){
+	  System.out.println("Put some kind of text");
+	  text = scn.nextLine();
+	}
+	void changeword(){
+		
+	}
+}
